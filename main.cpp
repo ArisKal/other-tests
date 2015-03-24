01af63180d433cf41be0bb827f4b81916d40d43e
Tas-sos <tasosnumberten@yahoo.gr>
2015-03-25 00:48:06 +0200

    Πλέον ΚΟΜΠΛΕ

--- main.cpp
+++ main.cpp
@@ -7,42 +7,26 @@ int main()
 	int d, m, y;
 	date d1;
 	date d2;
-	cout << "Enter date:";
-	    cin >> d;
-		cout << "Enter month:";
-		cin >> m;
-		cout << "Enter year:";
-		cin >> y;
+
+	d1.initDate();
+	d1.isLeap();
 		
 		
-		d1.setDay(d);
-		d1.setMonth(m);
-		d1.setYear(y);
-		d1.isLeap();
+	cout <<boolalpha<<d1.isLeap() <<endl;
+		
+	d1.printDate();
+	d1.nextDate();
 		
 		
-		cout <<boolalpha<<d1.isLeap() <<endl;
+	d2.initDate();
+	d2.setMonth(m);
+	d2.setYear(y);
 		
-		d1.printDate();
-		d1.nextDate();
-		
-		
-		cout << "Enter date:";
-		cin >> d;
-		cout << "Enter month:";
-		cin >> m;
-		cout << "Enter year:";
-		cin >> y;
-		
-		
-		d2.setDay(d);
-		d2.setMonth(m);
-		d2.setYear(y);
-		
-		cout << boolalpha << d2.isLeap() << endl;
+	cout << boolalpha << d2.isLeap() << endl;
 		
 	d2.printDate();
 	d2.nextDate();
+	
 	return 0;
 }
 
